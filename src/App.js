20f55d031fa4f31b3b6ffbe7389f66654c38164d
Jorge Mosqueda <mosqueda.ing@iclud.com>
2020-07-09 12:41:49 -0500

    Se añadieron los componentes sign-in-sign-up, sign-in y form-input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
