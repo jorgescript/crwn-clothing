99a1b3cb7a8f16007de1e2440fc22fa5b5ecaa4d
Jorge Mosqueda <mosqueda.ing@iclud.com>
2020-07-10 19:06:30 -0500

    Se añadio el componente cart-item

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+/* estilos */
+import "./cart-item.styles.scss";
+
+const CartItem = ({ cartItem: { imageUrl, price, name, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt={name} />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
